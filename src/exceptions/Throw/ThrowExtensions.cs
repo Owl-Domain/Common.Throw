03362f56f228ff1c841e7c406c7c4630b3045b93
Nightowl <nightowl286@protonmail.com>
2024-10-02 21:05:36 +0100

    * Small final tweaks. * Version bump.

--- src/exceptions/Throw/ThrowExtensions.cs
+++ src/exceptions/Throw/ThrowExtensions.cs
@@ -1,8 +1,13 @@
-﻿namespace OwlDomain.Common;
+namespace OwlDomain.Common;
 
 /// <summary>
 /// Contains extension methods on the <see cref="IThrowFor"/> interface for throwing exceptions.
 /// </summary>
+#if NET5_0_OR_GREATER
+[ExcludeFromCodeCoverage(Justification = "A little bit redundant to have auto generated tests for all of the auto generated throw methods.")]
+#else
+[ExcludeFromCodeCoverage()]
+#endif
 public static partial class ThrowExtensions
 {
 }
